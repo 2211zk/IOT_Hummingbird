cce2d9b81b407ff27a71effdfcac1afedae6a5ca
unknown <you@example.com>
2025-07-24 17:32:21 +0800

    ，美化页面

--- gin-vue-admin-main/web/src/components/standardCategorySelector/CategoryTable.vue
+++ gin-vue-admin-main/web/src/components/standardCategorySelector/CategoryTable.vue
@@ -40,8 +40,8 @@
     <!-- 分页 -->
     <div class="pagination-container">
       <el-pagination
-        v-model:current-page="currentPage"
-        v-model:page-size="pageSize"
+        :current-page="currentPage"
+        :page-size="pageSize"
         :total="total"
         :page-sizes="[10, 20, 50, 100]"
         layout="total, sizes, prev, pager, next, jumper"
