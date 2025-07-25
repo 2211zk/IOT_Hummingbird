1185893c99b16fdd1ccca90dea8d73c012a63045
unknown <you@example.com>
2025-07-25 15:48:18 +0800

    c端设备

---
+++ IOT_client/kratos_end/internal/service/equipment.go
@@ -0,0 +1,19 @@
+package service
+
+import (
+	"context"
+
+	pb "kratos_end/api/equipment/v1"
+)
+
+type EquipmentService struct {
+	pb.UnimplementedEquipmentServer
+}
+
+func NewEquipmentService() *EquipmentService {
+	return &EquipmentService{}
+}
+
+func (s *EquipmentService) ProductsList(ctx context.Context, req *pb.ProductsListReq) (*pb.ProductsListResp, error) {
+	return &pb.ProductsListResp{}, nil
+}
