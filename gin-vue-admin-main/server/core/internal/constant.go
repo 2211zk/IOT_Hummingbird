d3106b528b47db5140a2f311d040bcb349bc08ef
unknown <you@example.com>
2025-07-25 20:20:35 +0800

    修改

--- gin-vue-admin-main/server/core/internal/constant.go
+++ gin-vue-admin-main/server/core/internal/constant.go
@@ -2,7 +2,7 @@ package internal
 
 const (
 	ConfigEnv         = "GVA_CONFIG"
-	ConfigDefaultFile = "gin-vue-admin-main/server/config.yaml"
+	ConfigDefaultFile = "config.yaml"
 	ConfigTestFile    = "config.test.yaml"
 	ConfigDebugFile   = "config.debug.yaml"
 	ConfigReleaseFile = "config.release.yaml"
